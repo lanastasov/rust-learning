fffe378e7080dba08092c14f209d5972cac0dc57
lanastasov <lubomir.anastasov@gmail.com>
2019-01-25 17:20:36 +0200

    Q1

---
+++ vowel.rs
@@ -0,0 +1,14 @@
+use std::io;
+// Q1. Find an alphabet is vowel or not.
+fn main() {
+	let mut v=String::new();
+	println!("Enter a Character");
+	io::stdin().read_line(&mut v).expect("Failed");
+	let v:char=v.trim().parse().expect("Failed");
+
+	if v=='a' || v=='e' || v=='i' || v=='o' || v=='u' {
+		println!("vowel");
+	}else {
+		println!("consontant");
+	}
+}
