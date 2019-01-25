3665d518eb388d581fb7f70846f487c81736b7eb
lanastasov <lubomir.anastasov@gmail.com>
2019-01-25 15:53:58 +0200

    ch.clear() to remove doubling up on ch variable aka ch=="yy"

---
+++ social.rs
@@ -0,0 +1,40 @@
+use std::io;
+
+fn main() {
+	let mut name=String::new();
+	let mut age=String::new();
+	let mut ch=String::new();
+
+	println!("Enter Name and Age");
+	io::stdin().read_line(&mut name).expect("Failed");
+	io::stdin().read_line(&mut age).expect("Failed");
+	let age:i32=age.trim().parse().expect("Failed");
+
+	println!("Do you want to create account");
+	io::stdin().read_line(&mut ch).expect("Failed");
+	ch=ch.trim().to_string();
+	if ch=="y" {
+		if age<10 {
+			println!("Your age is less");
+		} else {
+			println!("Your Account is craeted");
+			println!("Do you want to upload photo");
+			// ch is remembered and it is equal to ch==yy 
+			// that's why we need to clear it 
+			ch.clear();
+			io::stdin().read_line(&mut ch).expect("Failed");
+			ch=ch.trim().to_string();
+
+			if ch=="y" {
+				if age<13 {
+					println!("You cannot upload photo");;
+				} else {
+					println!("You can upload photo");
+				}
+			} else {
+				println!("Thanks for Visiting");
+			}
+		}
+	}
+
+}
