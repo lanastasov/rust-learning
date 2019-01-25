f262387fd848ce88aea78791cca6229c7b50bc53
lanastasov <lubomir.anastasov@gmail.com>
2019-01-25 15:04:50 +0200

    input string and convert it to int; a is shadowed

---
+++ input_from_user.rs
@@ -0,0 +1,8 @@
+use std::io;
+fn main() {
+	let mut a=String::new();
+	println!("Enter a String");
+	io::stdin().read_line(&mut a).expect("Failed");
+	let a:i32=a.trim().parse().expect("Failed");
+	println!("{}",a);
+}
